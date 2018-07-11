8149446777927bb27a2a45d6b6a27dbc4b199b86
Khizri Tataev <bodycat@gmail.com>
2018-07-12 02:31:21 +0300

    lesson-3 Create class Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Класс - такой же тип данных как числа или символы
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write( sym );
+        }
+    }
+}
+

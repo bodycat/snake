9b6980264f8c11a3ed59d1a4d24ae9bd9cf58181
Khizri Tataev <bodycat@gmail.com>
2018-07-18 11:34:07 +0300

    lesson-16 New info

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,74 +6,75 @@ using System.Threading;
 
 namespace Snake
 {
-	class Program
-	{
-		static void Main( string[] args )
-		{
-			Console.SetBufferSize( 130, 130 );
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(130, 130);
 
-			Walls walls = new Walls( 80, 25 );
-			walls.Draw();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
-			// Отрисовка точек			
-			Point p = new Point( 4, 5, '*' );
-			Snake snake = new Snake( p, 4, Direction.RIGHT );
-			snake.Draw();
+            // Отрисовка точек			
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
-			FoodCreator foodCreator = new FoodCreator( 80, 25, '$' );
-			Point food = foodCreator.CreateFood();
-			food.Draw();
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
 
-			while (true)
-			{
-				if ( walls.IsHit(snake) || snake.IsHitTail() )
-				{
-					break;
-				}
-				if(snake.Eat( food ) )
-				{
-					food = foodCreator.CreateFood();
-					food.Draw();
-				}
-				else
-				{
-					snake.Move();
-				}
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
 
-				Thread.Sleep( 100 );
-				if ( Console.KeyAvailable )
-				{
-					ConsoleKeyInfo key = Console.ReadKey();
-					snake.HandleKey( key.Key );
-				}
-			}
-			WriteGameOver();
-			Console.ReadLine();
-      }
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+            }
+            WriteGameOver();
+            Console.ReadLine();
+        }
 
 
-		static void WriteGameOver()
-		{
-			int xOffset = 25;
-			int yOffset = 8;
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.SetCursorPosition( xOffset, yOffset++ );
-			WriteText( "============================", xOffset, yOffset++ );
-			WriteText( "И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++ );
-			yOffset++;
-            WriteText( "Практика ООП C#          ", xOffset + 2, yOffset++ );
-            WriteText( "Преподаватель GeekBrains:", xOffset + 2, yOffset++ );
-			WriteText( " Евгений Картавец        ", xOffset + 1, yOffset++ );
-            WriteText( " Выполнил:               ", xOffset + 1, yOffset++ );
-            WriteText( " Хизри Татаев            ", xOffset + 1, yOffset++ );
-			WriteText( "============================", xOffset, yOffset++ );
-		}
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("=================================", xOffset, yOffset++);
+            WriteText("       G E E K B R A I N S        ", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText(" Практика ООП Snake C#            ", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Преподаватель: Евгений Картавец  ", xOffset + 2, yOffset++);
+            WriteText(" Ученик: Татаев Хизри            ", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("   И Г Р А    О К О Н Ч Е Н А     ", xOffset + 1, yOffset++);
+            WriteText("=================================", xOffset, yOffset++);
+        }
 
-		static void WriteText( String text, int xOffset, int yOffset )
-		{
-			Console.SetCursorPosition( xOffset, yOffset );
-			Console.WriteLine( text );
-		}
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
 
-	}
+    }
 }
